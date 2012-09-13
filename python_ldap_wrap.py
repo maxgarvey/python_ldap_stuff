2f1351961c07519750d3cf37aeaa9f89cd8f8736
maxgarvey <maxgarvey@gmail.com>
2012-09-13 16:21:16 -0700

    got it working. changed modify and the cred object

--- python_ldap_wrap.py
+++ python_ldap_wrap.py
@@ -1,34 +1,94 @@
 #/tutorial.py
-import ldap
 import ldap.modlist as modlist
+import ldap
+import os
 
 ######
 '''this is the "conf" section'''
 ######
 
-server     = "idmdevl.oit.pdx.edu"
-username   = ""
-password   = ""
+class credentials(object):
+    '''a class to hold credentials.'''
+    def __init__(self):
+        '''init method.'''
+        #default values.
+        self.creds = {
+            'server':'idmdevl.oit.pdx.edu',
+            'username':'uid=magarvey,ou=people,dc=pdx,dc=edu',
+            'password':'',
+        }
+        #check for conf file.
+        if 'creds.py' in os.listdir(os.getcwd()):
+            import creds as cred
+            self.edit_creds(cred.creds['username'],
+                cred.creds['password'], cred.creds['server'])
+            del(cred)
+            print('creds.py file found. setting credentials accordingly:'+\
+                '\ncreds = {}'.format(self.creds))
+
+    def __str__(self):
+        '''to string method.'''
+        return str(self.creds)
+
+    def edit_creds(self, username, password, server=''):
+        '''this function will change the credentials that the ldap
+            connection object will use in trying to connect.
+
+            @params:
+                username - the username to use.
+                password - the password to use.
+                server(=server(as defined above)) - the server to bind to.
+        '''
+        self.creds['username'] = username
+        self.creds['password'] = password
+        if server is not '':
+            self.creds['server']   = server
+
+    def edit_username(self, username=''):
+        '''setter for username.
+
+        @params:
+            username - the username to set.'''
+        if username is not '':
+            self.creds['username'] = username
+
+    def edit_password(self, password=''):
+        '''setter for username.
+
+        @params:
+            password - the password to set.'''
+        if password is not '':
+            self.creds['password'] = password
+
+    def edit_server(self, server=''):
+        '''setter for server.
+
+        @params:
+            server - the server to set.'''
+        if server is not '':
+            self.creds['server'] = server
+
+#default object created
+my_creds = credentials()
 
 ######
 '''here are some methods to facilitate a subset of ldap operations.'''
 ######
 
-def connect(servername=server):
+def connect(creds_obj=my_creds):
     '''this method will return a connection object.
         Takes a single param, specified by default in this script.
 
         @params:
-            servername(={})- the name of the server to connect to'''\
-                .format(server)
+            servername() - the name of the server to connect to'''
 
     try:
-        l = ldap.open(servername)
-        print "binding to server: {}".format(servername)
-        l.simple_bind_s(username, password)
+        l = ldap.open(creds_obj.creds['server'])
+        print "binding to server: {}".format(creds_obj.creds['server'])
+        l.simple_bind_s(my_creds.creds['username'], my_creds.creds['password'])
         return l
     except Exception, error:
-        print "error connecting to server: {}\n\t{}".format(servername, error)
+        print "error connecting to server: {}\n\t{}".format(creds_obj.creds['server'], error)
         return None
 
 def disconnect(l, delete=False):
@@ -46,11 +106,11 @@ def _modify(l, existing_dn, before_change_dict, after_change_dict):
         to have the attributes in after dict.
 
         @params:
-            l - an ldap connection.
-            existing_dn - a dn for the record you want to change.
+            l                  - an ldap connection.
+            existing_dn        - a dn for the record you want to change.
             before_change_dict - dict of attrs+values you would like to 
                 change for the given record.
-            after_change_dict - dict of the attrs+values to change to.'''
+            after_change_dict  - dict of the attrs+values to change to.'''
 
     try:
         this_modlist = modlist.modifyModlist(before_change_dict,
@@ -66,8 +126,8 @@ def _search(l, search, baseDN='ou=people,dc=pdx,dc=edu', scope=ldap.SCOPE_SUBTRE
     '''this method will return a results id based on the search specified.
 
         @params:
-            l - an ldap connection.
-            search - a string or list of the search filters.
+            l       - an ldap connection.
+            search  - a string or list of the search filters.
             baseDN(='ou=people,dc=pdx,dc=edu') - the base distinguished 
                 name to search from.
             scope(=ldap.SCOPE_SUBTREE) - the type of search scope to use.'''
@@ -87,7 +147,7 @@ def get_results(l, r_id):
     '''returns the results for a specific query, specified by r_id.
 
         @params:
-            l = an ldap connection.
+            l    = an ldap connection.
             r_id = the id of the results you are looking for.'''
 
     try:
@@ -95,26 +155,59 @@ def get_results(l, r_id):
     except Exception, error:
         print("Error retrieving results:{}\n\t{}".format(Exception, error))
 
-def modify(existing_dn, before_dict, after_dict):
-    '''wraps the _modify method. cleans up.'''
-    ldap_obj = connect()
+def modify(existing_dn, before_dict, after_dict, creds_obj=None):
+    '''wraps the _modify method. cleans up.
+
+        @params:
+            existing_dn - the dn of the record you want to modify.
+            before_dict - a key:value dict of fields to be changed and their
+                initial values.
+            after_dict  - a key:value dict of fields to be changed and the
+                values they should have afterwards.'''
+    if creds_obj is not None:
+        try:
+            ldap_obj = connect(creds_obj)
+        except Exception, err:
+            print 'error with specified config: {}\n\t{}'\
+                .format(Exception, error)
+            try:
+                ldap_obj = connect()
+            except Exception, error:
+                print 'error with default config: {}\n\t{}'\
+                    .format(Exception, error)
     r_id = _modify(ldap_obj, existing_dn, before_dict, after_dict)
     results = ldap_obj.result(r_id)
     return results
 
-def search(search, search_params={}):
+def search(search_in, search_params={}, creds_obj=None):
     '''does all of the search stuff in one fell swoop. then cleans up.
 
         @params:
             search - the search filter/s.'''
 
-    ldap_obj = connect()
+    #debug
+    #print 'search_in:{}\n\ttype:{}'.format(search_in,type(search_in))
+    #print 'search_params:{}\n\ttype:{}'.format(search_params,type(search_params))
+    #print 'creds_obj:{}\n\ttype:{}'.format(creds_obj,type(creds_obj))
+
+    if creds_obj is not None:
+        try:
+            ldap_obj = connect(creds_obj)
+        except Exception, error:
+            print 'error with specified config: {}\n\t{}'\
+                .format(Exception, error)
+            try:
+                ldap_obj = connect()
+            except Exception, error:
+                print 'error with default config: {}\n\t{}'\
+                    .format(Exception, error)
+
+    has_base_dn = False
+    has_scope = False
 
     #unpack and analyze the keys of the optional param
     for key in search_params.keys():
-        has_base_dn = False
-        has_scope = False
-        if key.lower() == 'baseDN':
+        if key.lower() == 'basedn':
             has_base_dn = True
             base_dn = search_params[key]
         if key.lower() == 'scope':
@@ -125,15 +218,16 @@ def search(search, search_params={}):
                 scope = ldap.SCOPE_BASE
             else:
                 scope = ldap.SCOPE_SUBTREE
+
     #different cases for specified search fields
     if has_base_dn and has_scope:
-        r_id = _search(ldap_obj, search, base_dn, scope)
+        r_id = _search(ldap_obj, search_in, base_dn, scope)
     elif has_base_dn:
-        r_id = search(ldap_obj, search, base_dn)
+        r_id = _search(ldap_obj, search_in, base_dn)
     elif has_scope:
-        r_id = search(ldap_obj, search, 'ou=people,dc=pdx,dc=edu', scope)
+        r_id = _search(ldap_obj, search_in, 'ou=people,dc=pdx,dc=edu', scope)
     else:
-        r_id = _search(ldap_obj, search)
+        r_id = _search(ldap_obj, search_in)
 
     #retrieve results
     results = get_results(ldap_obj, r_id)
